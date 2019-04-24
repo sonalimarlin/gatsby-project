ca2f4d5d08d03076e013fcd2099586ff01dc7040
sonalimarlin <sonali.marlin@gmail.com>
2019-04-23 20:11:11 -0600

    clone repo and add some text

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Layout from '../components/layout';
 const IndexPage = () => {
     return (
         <Layout>
-            <h1>Aliquip ex ea commodo</h1>
+            <h1>Aliquip Non Seqitor</h1>
             <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
             <p>Need a developer? <Link to="/contact">Contact Me!</Link></p>
         </Layout>
